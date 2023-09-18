1a30c012926673ba1dcdaf37f6c71d028649e43c
ErickColon <142958153+ErickColon@users.noreply.github.com>
2023-09-17 19:42:07 -0500

    Add files via upload

---
+++ PS3P4.py
@@ -0,0 +1,10 @@
+#Input Phase
+First_name = input("Enter your first name")
+Number_of_steps = float(input("Enter number of steps"))
+
+#Process Phase
+Calories_burned = Number_of_steps * .25
+
+#Output Phase 
+print(First_name)
+print("Calorie's burned is ", Calories_burned)
