476c2bf2565bd19917286ce92fd92e6924db2096
ErickColon <142958153+ErickColon@users.noreply.github.com>
2023-11-25 23:05:55 -0600

    Add files via upload

---
+++ PS13.py
@@ -0,0 +1,31 @@
+def dll (mylist):
+  n = int(input("Number of items for your list"))
+  for n in range (0, n, 1) :
+  s = int(input ("Enter an integer"))
+  mylist.append(s) return mylist
+  def displaylist(mylist):
+  for item in mylist:
+    print(item)
+
+
+mylist = []
+
+mylist = d11(mylist) displaylist(mylist) 
+print(mylist)
+
+#2
+mylist.insert(0,99)
+print(mylist)
+#3
+mylist[0] = 100
+print(mylist)
+#4
+mylist2 = ["'Jones", "Adams", "Baker", "Smith"]
+mylist3 = [Rizzo",
+"Baez", "Bryant",
+"Baker"]
+mylist2.extend(mylist3)
+print(mylist2
+ #5
+mylist2.remove ("Jones")
+print (mylist2)
